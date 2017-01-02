4cab157178cc6f2c774176c09a262559cb318f0e
Victor Emanuel Del Castillo <emanuel.delcastillo@gmail.com>
2017-01-01 21:27:04 -0400

    Adds feature: changes label's content based on user input

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
   // MARK: Properties
   @IBOutlet weak var nameTextField: UITextField!
@@ -17,12 +17,29 @@ class ViewController: UIViewController {
   // MARK: UIViewController
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Handle the text field's user input through delegate callbacks.
+    nameTextField.delegate = self
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+  
+  // MARK: UITextFieldDelegate
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    
+    // Hide the keyboard
+    textField.resignFirstResponder()
+    return true
+  }
+  
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    
+    // Replaces the content of label with text field's value
+    nameMealLabel.text = textField.text
   }
   
   // MARK: Actions
