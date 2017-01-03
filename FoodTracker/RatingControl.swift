c442d8a19240b43ba132d3b96e25dfd9af44804a
Victor Emanuel Del Castillo <emanuel.delcastillo@gmail.com>
2017-01-03 18:34:19 -0400

    Adds a Custom View to the scene

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,22 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Emanuel Del Castillo on 1/3/17.
+//  Copyright © 2017 Del Castillo. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+  
+  // MARK: Initialization
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init(coder: NSCoder) {
+    super.init(coder: coder)
+  }
+
+}
